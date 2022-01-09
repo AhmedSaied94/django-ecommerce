2764eb598e520d2018707c6b4f4d23ed19e0e5ec
Ahmed Saied <ahmed.saeed311294@gmail.com>
2022-01-10 01:09:11 +0200

    added main templates and home template

--- Ecommerce/models.py
+++ Ecommerce/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    img = models.ImageField(upload_to='items/imgs', null=True, blank=True)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-added_date']
+
+    def __str__(self):
+        return self.title
+
+class OrderItem(models.Model):
+    item = models.ForeignKey('Item', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item.title
+
+
+class ShopingCard(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField('OrderItem')
+    start_date = models.DateTimeField(auto_now_add=True)
+    orderd_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-orderd_date']
+    
+    def __str__(self):
+        return self.user.username
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+
+def items_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'Ecommerce/items-list.html', context)
